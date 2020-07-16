a1d3d4fffacbda67acaff551f4bb152a064b807c
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2020-07-16 16:50:37 +0600

    revers statement javascript

---
+++ revers1.js
@@ -0,0 +1,14 @@
+function reverseString(str){
+    var reverse = "";
+    for(var i=0; i < str.length; i++){
+        var char = str[i];
+        reverse = char + reverse;
+    }
+    return reverse;
+}
+
+
+var statement = "Hello boss, how are you?"
+
+var forAlien = reverseString(statement);
+console.log(forAlien);
